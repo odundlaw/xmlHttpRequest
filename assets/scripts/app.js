5843a1455a52e495f42056237fb6e6a38b7996c3
odundlaw <odunayoshittu55@gmail.com>
2022-08-03 21:35:14 +0100

    learning http request with xml http

---
+++ assets/scripts/app.js
@@ -0,0 +1,84 @@
+const postTemplate = document.getElementById("single-post");
+const listItem = document.querySelector(".posts");
+const form = document.querySelector("#new-post form");
+const fetchBtn = document.querySelector("#available-posts button");
+
+async function httpRequest(method, url, data) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+
+    xhr.send(data && JSON.stringify(data));
+
+    xhr.onload = function () {
+      resolve(JSON.parse(xhr.response));
+    };
+
+    xhr.onerror = function () {
+      reject(xhr.response);
+    };
+  });
+
+  return promise;
+}
+
+async function fetchPosts() {
+  try {
+    const postData = await httpRequest(
+      "GET",
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+
+    for (const post of postData) {
+      const template = document.importNode(postTemplate.content, true);
+      template.querySelector("h2").textContent = post.title.toUpperCase();
+      template.querySelector("p").textContent = post.body;
+      template.querySelector("li").id = post.id;
+      listItem.append(template);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    userId,
+    title,
+    content,
+  };
+
+  const response = await httpRequest(
+    "POST",
+    "https://jsonplaceholder.typicode.com/posts",
+    post
+  );
+  console.log(response);
+}
+
+async function deletePost(postId) {
+  const response = await httpRequest(
+    "DELETE",
+    `https://jsonplaceholder.typicode.com/posts/${postId}`
+  );
+  console.log(response);
+}
+
+fetchBtn.addEventListener("click", fetchPosts);
+
+listItem.addEventListener("click", (event) => {
+  if(event.target.textContent === "DELETE"){
+    const id = event.target.closest("li").id;
+    deletePost(id)
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event.currentTarget);
+  const enteredTitle = event.currentTarget.querySelector("#title").value;
+  const enteredContent = event.currentTarget.querySelector("#content").value;
+
+  createPost(enteredTitle, enteredContent);
+});
